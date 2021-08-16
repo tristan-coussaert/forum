d804a7d2263d1e5e706146b83a654cbdad714dfb
Tristan <mytic12@hotmail.fr>
2021-08-16 15:10:28 +0200

    :sparkles: Add page 404

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Login from './Login'
 import Register from './Register'
 import Post from './Post'
 import Header from './Header'
+import Page404 from './Page404'
 import Home from './Home'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import {auth} from './config'
 import {useEffect} from 'react'
 import { useStateValue } from './Context';
@@ -46,8 +47,13 @@ function App() {
             <Header />
             <Post />
         </Route>
+        <Route>
+            <Redirect to="/404"/>
+            <Header />
+            <Page404 />
+        </Route>
     </Switch>
-    </Router>
+    </Router>     
   );
 }
 
---
+++ src/Page404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Page404.css'
+
+function Page404() {
+    return (
+        <div className="page404">
+            <h1>Désolé, la page que vous cherchez n'existe pas.</h1>
+        </div>
+    )
+}
+
+export default Page404;
