ccb1846e66103a95bffdf6fdf20bbbca44c09be8
Tristan <mytic12@hotmail.fr>
2021-08-18 15:05:12 +0200

    :sparkles: :lipstick: Add searchbar & Modif style

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,10 @@ import { Link } from "react-router-dom";
 function Home(){
     const [{loggedinuser}, dispatch] = useStateValue();
     const [topics, setTopics] = useState([]);
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+
     useEffect(() => {   
         db.collection("topics")
           .orderBy("createdAt", "desc")
@@ -53,17 +57,41 @@ function Home(){
       return <Link to={"/login"} className="header__link"><AddNewTopic /></Link>;
     }
 
-    
+    const filterPosts = (topics, query) => {
+      if (!query) {
+          return topics;
+      }
+      return topics.filter((post) => {
+          const postName = post.title.toLowerCase();
+          return postName.includes(query);
+      });
+  };
+
+  const filteredPosts = filterPosts(topics, searchQuery);
+
 
       return (
           <div className="home">
+            <div className="element">
+          <div className="searchbar">
+          <input
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+            type="text"
+            id="header-search"
+            placeholder="Rechercher un sujet"
+            name="s"
+            class="searchbar__input"
+        />
+          </div>
           <div className="button">
-          <AddTopic />
+          <AddTopic  />
           <Button onClick={refreshPage} colorScheme="blue" className="button__Refresh" >Actualiser</Button>
+          </div>
           </div>
           <Container maxW="md" centerContent p={8}>
             <VStack spacing={8} w="100%">
-              {topics.map((post) => (
+              {filteredPosts.map((post) => (
                 <Topic post={post} key={post.id} />
               ))}
             </VStack>
